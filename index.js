4efff68cfca47580245b1f65ed800b9199126f90
Scott Norvell <scottlnorvell@gmail.com>
2020-05-08 14:47:08 -0400

    implement search

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict';
 const alfy = require('alfy');
+const Fuse = require('fuse.js');
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const data = require('./data.json');
+
+const options = {
+	keys: ['name', 'tags'],
+};
+
+const fuse = new Fuse(data, options);
+
+const mapResult = ({ name, imagePath, emoji, slug }) => ({
+	title: name,
+	uuid: slug,
+	icon: {
+		path: `./images/${imagePath}`,
+	},
+	arg: emoji,
+});
+
+alfy.output(
+	fuse.search(alfy.input).map(
+		({ item }) => mapResult(item),
+	)
+);
